175be289ffd5b7e91d897f4416eb79f88595cda9
Myia Lamy <mlamy@usf.edu>
2024-09-21 02:30:42 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -6,3 +6,16 @@ const employees = [
     {name: 'Terry', shifts: [{ day: 'Friday', hours: 8}, { day: 'Saturday', hours: 4}]},
     {name: 'Braylin', shifts: [{ day: 'Monday', hours: 7}]}
 ];
+
+// Task 2: Create a Function to Display Employee Shift Details
+
+function displayEmployeeShifts(employee) {
+    console.log(`Employee: ${employee.name}`); // Used to display employee name. 
+        employee.shifts.forEach(shift => {
+            console.log(`Day: ${shift.day}, Hours: ${shift.hours}`); // Used to display day and hours employee is scheduled for. 
+        });
+}
+let employeeSchedule = displayEmployeeShifts(employees[0]); // Declare which employee's schedule to view with a number. "0" is the first employee. 
+console.log(employeeSchedule); // Display schedule 
+
+
